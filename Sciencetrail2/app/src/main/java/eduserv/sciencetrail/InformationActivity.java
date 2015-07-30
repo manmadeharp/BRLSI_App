46a368162efcbf466db8eace8b41cdd09f4278ea
strangeslop <finbarcowan@yahoo.co.uk>
2015-07-30 14:38:26 +0100

    uploads
    commiting

--- Sciencetrail2/app/src/main/java/eduserv/sciencetrail/InformationActivity.java
+++ Sciencetrail2/app/src/main/java/eduserv/sciencetrail/InformationActivity.java
@@ -1,6 +1,8 @@
 package eduserv.sciencetrail;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,6 +18,8 @@ public class InformationActivity extends ActionBarActivity {
     private TextView lblTtitle;
     private MapInformationObject mapItem;
     private RelativeLayout background;
+    private ImageView image;
+    private TextView longdescription;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,26 +27,30 @@ public class InformationActivity extends ActionBarActivity {
 
         mapItem = (MapInformationObject)getIntent().getSerializableExtra("MAP_ITEM");
         background = (RelativeLayout)findViewById(R.id.background);
-
+        image = (ImageView)findViewById((R.id.image));
         lblTtitle = (TextView)findViewById(R.id.title);
-
+        longdescription = (TextView)findViewById(R.id.longdescription);
 
         lblTtitle.setText(mapItem.getTitle());
+        longdescription.setText(mapItem.getLongDescription());
 
-        if (mapItem.getTrialName().equals("TREE")){
+        if (mapItem.getTrialName().equals("tree")){
             //set background color
-            background.setBackgroundColor(Color.parseColor("#cc0000"));
+            image.setImageResource(R.drawable.tree);
+            background.setBackgroundColor(Color.parseColor("#2ecc71"));
             // set image
         }
-        if (mapItem.getTrialName().equals("SCIENCE")){
+        if (mapItem.getTrialName().equals("science")){
             //set background color
-            background.setBackgroundColor(Color.parseColor("#cc0500"));
+            background.setBackgroundColor(Color.parseColor("#3498db"));
             // set image
+            image.setImageResource(R.drawable.telesc);
         }
-        if (mapItem.getTrialName().equals("ADELARD")){
+        if (mapItem.getTrialName().equals("adelard")){
             //set background color
-            background.setBackgroundColor(Color.parseColor("#cc8000"));
-            // set image
+            background.setBackgroundColor(Color.parseColor("#e74c3c"));
+            image.setImageResource(R.drawable.medieval);
+
         }
 
     }
